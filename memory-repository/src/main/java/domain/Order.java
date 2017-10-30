d84289f5fa29d328565c63e61a87d235f5fa8018
Dezmerean Robert <dezmereanrobert@gmail.com>
2017-10-30 20:33:55 +0200

    add order

---
+++ memory-repository/src/main/java/domain/Order.java
@@ -0,0 +1,66 @@
+package domain;
+
+public class Order {
+    private int id;
+    private int price;
+    private int quantity;
+
+    public Order(int id, int price, int quantity) {
+        this.id = id;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        if (id != order.id) return false;
+        if (price != order.price) return false;
+        return quantity == order.quantity;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + price;
+        result = 31 * result + quantity;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
